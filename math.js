d81fb99df3e507683297f42882324764bce47269
Luiz Felipe <doti0026@gmail.com>
2026-03-03 00:28:40 +0000

    O Teste

---
+++ math.js
@@ -0,0 +1,32 @@
+// add(a, b): Retorna a soma de a e b.
+// subtract(a, b): Retorna a diferença entre a e b.
+// multiply(a, b): Retorna o produto de a e b.
+// divide(a, b): Retorna a divisão de a por b.
+
+function add(a, b) {
+
+return a+b;
+}
+
+
+function subtract(a, b) {
+
+return a-b;
+}
+
+function multiply(a, b) {
+
+return a*b;
+}
+
+function divide(a, b) {
+
+return a/b;
+}
+
+module.exports = {
+    add,
+    divide,
+    multiply,
+    subtract
+}
---
+++ tests/math.test.js
@@ -0,0 +1,5 @@
+const math=require('../math');
+
+test('soma 2 + 3 igual a 5', () => {
+    expect(math.add(2, 3)).toBe(5);
+});
